c266e28be16b86f20459da9d7d1e74d676b1c9f3
Akbarss <kbrgulyamov@gmail.com>
2024-08-29 10:19:21 +0500

    f

--- Lesson-6/main.js
+++ Lesson-6/main.js
@@ -6,6 +6,17 @@ for (let i = 0; i < 9; i++) {
 
 console.log(str);
 
+// let str = "";
+// let i = 0;
+
+// while (i < 9) {
+//   str = str + i;
+//   i++;
+// }
+
+// console.log(str);
+// Вывод: "012345678"
+
 // Пример 2: Итерация по строке
 
 const text = "Hello";
